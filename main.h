3e827fc4e14abe9969cfa8bd7b59946079a35117
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-11 14:46:34 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+/**
+ * struct convert - define a structure 
+ *
+ * @oma: the operator
+ * @f: the function associated
+ */
+
+struct convert
+{
+	char *oma;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
