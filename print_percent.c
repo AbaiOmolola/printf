a892f61ea8c2441e46bc20136de22811a5f5100e
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-11 15:43:29 -0700

    print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include <main.h>
+
+/**
+ * print_percent - prints a percent symbol
+ * @list: list of argument
+ * Return: will return amount of characters
+ */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_putchar('%');
+	return (1)
+}
