2c4e09a611458b1ae561bdb4134142fd5f5dc268
AbaiOmolola <105018454+AbaiOmolola@users.noreply.github.com>
2022-07-12 05:33:23 +0100

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -13,7 +13,7 @@ int print_string(va_list list)
 	str = va_arg(list, char *);
 	if (str == NULL)
 		str = "(null)";
-	for (i = 0; str[i] = != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	return (i);
 }
