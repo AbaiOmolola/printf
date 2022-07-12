d18c0161d4739be72b6390ee99854291413ecb12
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-11 18:17:45 -0700

    print_octal.c

---
+++ print_octal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_octal - prints the numeric representation of number in octal base
+ * @list: list all of the argument passed
+ * Return: Number of symbol printed stdout
+ */
+int print_octal(va_list list)
+{
+	unsigned int num;
+	int len;
+	char *octal_rep;
+	char *rev_str;
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (_putchar('0'));
+	if num (num < 1)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		octal_rep[len] = (num % 8) + 48;
+		num = num / 8;
+
+	}
+	octal_rep[len] = '\0';
+	rev_str = rev_string(octal_rep);
+	if (rev_str == NULL)
+		return (-1);
+	write_base(rev_str);
+	free(octal_rep);
+	free(rev_str);
+	return (len);
+}
