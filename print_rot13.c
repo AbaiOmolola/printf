cf6a8ad44a269449c83e7a45009491067e3e1c40
AbaiOmolola <abaimaryomolola@gmail.com>
2022-07-11 19:37:57 -0700

    print_rot13.c

---
+++ print_rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rot13 - prints a string using rot13
+ * @list: list of argument
+ * Return: length of printed string
+ */
+int print_rot13(va_list list)
+{
+	int i;
+	int x;
+	char *str;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+		return (-1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 52; x++)
+		{
+			if (str[i] == str[x])
+			{
+				_putchar(u[x]);
+				break;
+			}
+		}
+		if (x == 53)
+			_putchar(str[i]);
+	}
+	retutn (i);
+}
